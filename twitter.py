d93ee934f9cb4fbede37a1100e1f09f97da16331
Widy <34003602+widyastuti@users.noreply.github.com>
2020-08-03 07:34:52 +0700

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -29,7 +29,7 @@ class TwitterClient(object):
             self.retweets_only = retweets_only
             self.with_sentiment = with_sentiment
             self.api = tweepy.API(self.auth, timeout=100)
-            self.tweet_count_max = 3            
+            self.tweet_count_max = 100            
             self.tweet_mode = tweet_mode
         except:
             print("Error: Authentication Failed")
